2203b2e3fab69e9c9f046fcc4b465d1c02f94b98
Mark Jefferson <markjeffersonhidalgo08@gmail.com>
2024-06-18 10:11:57 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const counterText = document.querySelector(".count-text");
+const btns = document.querySelectorAll(".btn");
+
+let count = 0;
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const currentBtn = e.currentTarget.classList;
+
+    if (currentBtn.contains("dec-btn")) {
+      count--;
+    } else if (currentBtn.contains("inc-btn")) {
+      count++;
+    } else {
+      count = 0;
+    }
+
+    counterText.textContent = count;
+  });
+});
